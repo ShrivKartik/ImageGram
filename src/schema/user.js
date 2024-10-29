0980b2f5a2c01e49a993ebf2d924b85ada43a137
Kartikey Shrivastav <shrivastavkartikey@gmail.com>
2024-10-29 22:53:59 +0530

    user schema added

---
+++ src/schema/user.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5
+    },
+
+    email:{
+        type: String,
+        required: true,
+        minLength: 5,
+        validate: {
+            validator: function (emailValue){
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue);
+            },
+            message: 'Invalid mail format'
+        }
+    },
+
+    password:{
+        type: String,
+        required: true,
+        minLength: 8
+    }
+})
+
+const user = mongoose.model("User", userSchema);
+
+export default user;
